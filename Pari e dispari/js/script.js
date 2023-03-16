ea67122a52b5933a6f9ce5b502db62b0b1f7e74f
nespolisimmaco <snespoli@yahoo.com>
2023-03-16 17:28:36 +0100

    Add: sum the two numbers and check with a function if the sum is odd or even

--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -8,12 +8,22 @@
 const userChoice = prompt("Ciao utente! Scegli: pari o dispari?").toLowerCase();
 console.log(userChoice);
 let userNumber = parseInt(prompt("Adesso inserisci un numero da 1 a 5"));
+// SE il numero non è nel range
 while (5 > userNumber < 1) {
     userNumber = parseInt(prompt("Il numero non è compreso nel range, reinserisci"));
 }
 console.log("User number", userNumber);
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 const computerNumber = getRndInteger(1, 5);
 console.log("Computer number", computerNumber);
+// Sommo i due numeri
+const sum = userNumber + computerNumber;
+console.log("Somma", sum);
+const oddOrEvenCheck = oddOrEven(sum);
+console.log(oddOrEvenCheck);
+
+////////////////////
+// FUNCTIONS
 /**
  * Description
  * @param {number} min
@@ -23,3 +33,16 @@ console.log("Computer number", computerNumber);
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
+
+/**
+ * Description
+ * @param {number} numbersSum
+ * @returns {string} pari o dispari
+ */
+function oddOrEven(numbersSum) {
+    if (numbersSum % 2 === 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+}
